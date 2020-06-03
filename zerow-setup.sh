cf311dadcec3df893f384c34b24945245d014dc0
DHkim <ultraraptor@naver.com>
2020-06-03 09:40:53 +0900

    good!'

---
+++ zerow-setup.sh
@@ -0,0 +1,8 @@
+sudo apt-get update
+sudo apt-get install libjpeg-dev -y
+sudo apt-get install zlib1g-dev -y
+sudo apt-get install libfreetype6-dev -y
+sudo apt-get install liblcms1-dev -y
+sudo apt-get install libopenjp2-7 -y
+sudo apt-get install libtiff5 -y
+pip3 install pillow
